99319f0e5c4144a27af18d88be01506a37b5b787
pooja <spoojashekar@gmail.com>
2023-08-28 17:29:35 -0700

    theme changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
 import Home from "./pages/Home";
 import { ThemeProvider } from "@mui/material/styles";
-import { theme } from "./Foundation/theme";
+//import { theme } from "./Foundation/theme";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
+    //<ThemeProvider theme={theme}>
       <div className="App">
         <Home />
       </div>
-    </ThemeProvider>
+   // </ThemeProvider>
   );
 }
 
