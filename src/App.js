2d4ea76e841494eafa849449022791498d185b46
pcparagc <pcparagc@gmail.com>
2023-08-30 12:53:36 -0700

    ReactFLow Added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import Home from "./pages/Home";
 import { ThemeProvider } from "@mui/material/styles";
 import { appTheme } from "./foundation/theme";
 import Layout from "./layouts/Layout";
+import CourseBlock from "./components/CourseBlock";
 
 function App() {
   return (
     <ThemeProvider theme={appTheme}>
       <Layout>
-        <Home />
+        <CourseBlock />
       </Layout>
     </ThemeProvider>
   );
