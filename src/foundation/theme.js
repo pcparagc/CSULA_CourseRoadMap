db7245f7ef0cd43ead9a4417809546cfe9f394df
poojashekar987 <65018684+poojashekar987@users.noreply.github.com>
2023-09-12 10:21:55 -0700

    Delete src directory

--- src/foundation/theme.js
+++
@@ -1,8 +0,0 @@
-import { orange } from "@mui/material/colors";
-import { createTheme } from "@mui/material/styles";
-
-export const theme = createTheme({
-  status: {
-    danger: orange[500],
-  },
-});
