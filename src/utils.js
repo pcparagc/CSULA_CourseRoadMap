7ad5c711dce6883ccf3246b0823bc5815fb1b195
pcparagc <pcparagc@gmail.com>
2023-09-16 19:38:54 -0700

    utils added

---
+++ src/utils.js
@@ -0,0 +1,42 @@
+export const getSemesterLabel = (semester) => {
+  switch (semester) {
+    case "fall":
+      return "F";
+    case "summer":
+      return "X";
+    case "spring":
+      return "S";
+    default:
+      return "";
+  }
+};
+
+export const getCourseType = (course_type) => {
+  switch (course_type) {
+    case "General Education":
+      return {
+        backgroundColor: "#FFBD01",
+        border: "2px solid #F7951E",
+      };
+    case "Lower Division":
+      return {
+        backgroundColor: "#A2D40B",
+        border: "2px solid #2AA438",
+      };
+    case "Upper Division":
+      return {
+        backgroundColor: "#9FDAE8",
+        border: "2px solid #28AAE3",
+      };
+    case "Senior Design":
+      return {
+        backgroundColor: "#D4F3ED",
+        border: "2px solid #28AAE3",
+      };
+    default:
+      return {
+        backgroundColor: "#A2D40B",
+        border: "2px solid #2AA438",
+      };
+  }
+};
